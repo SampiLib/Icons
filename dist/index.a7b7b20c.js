fd16ed035f67f512736a6058b3385cad4b33f562
Nicklas Nielsen <nicknielsen1995@hotmail.com>
2022-10-25 23:10:21 +0200

    Change testing framework to jest Fixes #2

--- dist/index.a7b7b20c.js
+++
@@ -1,5 +0,0 @@
-const variable = "Hello, World!";
-let div = document.getElementById("greeting");
-if (div) div.innerHTML = variable;
-
-//# sourceMappingURL=index.a7b7b20c.js.map
--- src/allIcons.ts
+++ src/allIcons.ts
@@ -1,5 +1,12 @@
+import * as icons from "./icons/index"
+
 
 
 export function allIcons() {
 
 }
+
+console.warn(icons);
+
+console.log('end');
+
