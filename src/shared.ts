24bf92c959f1b0e53eaaef8a6fdd2a70d1c9fd7f
Nicklas Nielsen <nicknielsen1995@hotmail.com>
2022-11-19 16:51:45 +0100

    Changed logic to make icons generate on fist call

--- src/shared.ts
+++ src/shared.ts
@@ -4,12 +4,16 @@ export interface SVGGenerator extends Function {
 }
 
 export function generateFunction(name: string, path: [string, ...string[]], type: string, icon: string) {
-    let svg = <SVGSVGElement>(new DOMParser().parseFromString(icon, 'image/svg+xml')).firstChild;
-    svg.setAttribute('icon', name);
-    svg.setAttribute('type', type);
-
+    let svg: SVGSVGElement | undefined;
     let func = <SVGGenerator>function () {
-        return <SVGSVGElement>svg.cloneNode(true);
+        if (svg) {
+            return <SVGSVGElement>svg.cloneNode(true);
+        } else {
+            svg = <SVGSVGElement>(new DOMParser().parseFromString(icon, 'image/svg+xml')).firstChild;
+            svg.setAttribute('icon', name);
+            svg.setAttribute('type', type);
+            return <SVGSVGElement>svg.cloneNode(true);
+        }
     };
     func.path = path;
     return func;
